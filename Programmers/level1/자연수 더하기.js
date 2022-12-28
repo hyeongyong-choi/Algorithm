12a9f33ed81056546cf20234200c35a4d969aa78
hyeongyong-choi <guddyd6761@naver.com>
2022-12-28 22:52:09 +0900

    자연수 더하기

---
+++ Programmers/level1/자연수 더하기.js
@@ -0,0 +1,12 @@
+
+function solution(n)
+{
+    let answer = 0;
+    let answer_s=String(n);
+
+    for(var i=0; i<answer_s.length; i++){
+        answer+=parseInt(answer_s[i]);
+    }
+    return answer;
+}
+
