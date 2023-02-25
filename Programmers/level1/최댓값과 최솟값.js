22508010f5df462d57a9dc7818b79a958f55de2b
hyeongyong-choi <guddyd6761@naver.com>
2023-02-25 23:21:08 +0900

    최댓값과 최솟값

---
+++ Programmers/level1/최댓값과 최솟값.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    let answer = s.split(" ")
+    
+    let max = Math.max(...answer)
+    let min = Math.min(...answer)
+    return min+" "+max;
+       
+    return answer;
+}
